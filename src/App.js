4f338ba529fb74912ac012466982cf3fe3dd10dc
Parth <parth.shiralkar@gmail.com>
2022-04-09 15:38:34 -0700

    Update site

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
 import logo from './logo.svg';
+import {useState,useEffect} from "react";
 import './App.css';
+import Player from './components/Player';
 
-function App() {
+const App=()=> {
+  const [songs,setSongs] = useState([
+    {
+        "title": "testing",
+        "artist": "notparth!",
+        "img_src": "./track_images/pexels-ekaterina-3128080.jpg",
+        "src": "./tracks/actual daftMASTER.wav"
+    },    {
+      "title": "testing 2",
+      "artist": "notparth!",
+      "img_src": "./track_images/pexels-ekaterina-3128080.jpg",
+      "src": "./tracks/actual daftMASTER.wav"
+  },
+]);
+
+const [currentSongIndex,setCurrentSongIndex] = useState(0);
+const [nextSongIndex,setNextSongIndex] = useState(currentSongIndex + 1);
+
+
+useEffect(()=>{
+  setNextSongIndex(()=>{
+  if (currentSongIndex + 1 >songs.length - 1 ){
+    return 0;
+  } else{
+    return currentSongIndex + 1;
+  }
+});
+},[currentSongIndex])
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          HAHA
-        </a>
-      </header>
+      GAGA
+      <Player currentSongIndex={currentSongIndex} setCurrentSongIndex={setCurrentSongIndex} nextSongIndex={nextSongIndex} songs={songs} />
     </div>
   );
 }
---
+++ src/components/Player.js
@@ -0,0 +1,85 @@
+//Player.js
+
+import React, { useState, useRef, useEffect } from "react";
+import PlayerDetails from "./PlayerDetails";
+import PlayerControls from "./PlayerControls";
+
+function Player(props) {
+  const audioElement = useRef(null);
+  const [isPlaying, setIsPlaying] = useState(false);
+
+  useEffect(() => {
+    if (isPlaying) {
+      audioElement.current.play();
+    } else {
+      audioElement.current.pause();
+    }
+  });
+
+  const SkipSong = (forwards = true) => {
+    if (forwards) {
+      props.setCurrentSongIndex(() => {
+        let temp = props.currentSongIndex;
+        temp++;
+
+        if (temp > props.songs.length - 1) {
+          temp = 0;
+        }
+
+        return temp;
+      });
+    } else {
+      props.setCurrentSongIndex(() => {
+        let temp = props.currentSongIndex;
+        temp--;
+
+        if (temp < 0) {
+          temp = props.songs.length - 1;
+        }
+
+        return temp;
+      });
+    }
+  };
+
+  return (
+    <>
+      <p>
+        <div className="text-anim">
+          <strong>Upcoming Song:</strong>
+        </div>
+
+        <div className="nextsong-details">
+          <img
+            src={props.songs[props.nextSongIndex].img_src}
+            alt={props.songs[props.nextSongIndex].title}
+            style={{ width: "4em", height: "auto" }}
+          />
+          <p>
+            <b>{props.songs[props.nextSongIndex].title} </b>&nbsp; by &nbsp;
+            <b>{props.songs[props.nextSongIndex].artist}</b>
+            {/* &nbsp; from album
+            &nbsp; */}
+            {/* <b>{props.songs[props.nextSongIndex].album}</b> */}
+          </p>
+        </div>
+      </p>
+      <div className="music-player">
+        <audio
+          src={props.songs[props.currentSongIndex].src}
+          ref={audioElement}
+        ></audio>
+        <PlayerDetails song={props.songs[props.currentSongIndex]} />
+
+        <PlayerControls
+          isPlaying={isPlaying}
+          setIsPlaying={setIsPlaying}
+          SkipSong={SkipSong}
+        />
+
+        {/* <h4>Lofi Music Player React </h4> */}
+      </div>
+    </>
+  );
+}
+export default Player;
